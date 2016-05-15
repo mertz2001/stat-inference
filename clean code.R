c6e7bb4fd65830ef7bfcfa5bbbd051b97133a946
mertz2001 <mertz200190@gmail.com>
2016-05-15 15:59:37 +1000

    Updated code inclusing models

--- clean code.R
+++ clean code.R
@@ -7,19 +7,22 @@ library(rgeos)
 library(maptools)
 library(spdep)
 library("rgdal")
-
+#install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable") # Should be installed using this path
+library("INLA")
+library("plyr")
+library("dplyr")
 ####################################### IMPORT DATA ################################################
 
 #Get the immunisation data:
-setwd("~/Documents/uni/STAT3013/Project/stat-inference")
+setwd("~/GitHub/stat-inference")
 immunisation_data <- read.csv("1 year fully immunised.csv")
 
 #Get the postcode area data:
-setwd("~/Documents/uni/STAT3013/Project/stat-inference/Postcode data/1270055003_poa_2011_aust_shape")
+setwd("~/GitHub/stat-inference/1270055003_poa_2011_aust_shape")
 Postcodes_areas <- readOGR(".", "POA_2011_AUST")
 #ignore the warning message: "dropping null geometries: 2514, 2515, 2516"
 
-setwd("/Users/jamesbailie/Documents/uni/STAT3013/Project/stat-inference")
+setwd("~/GitHub/stat-inference")
 
 ################################### FILTER POSTCODES BY NPs ########################################
 ###Remove postcodes that we do not have immunsiation data on
@@ -112,6 +115,87 @@ POAs_with_data[2,1]
 #      iff entry [i,j] is 1
 #   - Immunisation data - filtered by whether there is immunisation data & postal code area data - 
 #      is stored in Postcodes_with_area_and_immune_data
+
+#################################### RUNNING THE MODELS ###########################################
+
+# Definr the Graph/neigborhood matrix
+g = Postcode_neighbours
+
+# First Look at the data
+summary(immunisation_data)
+
+## Dropping NA's
+immunisation = left_join(POAs_with_data, immunisation_data, by=c("POA_CODE"="Postcode"))
+
+# Changing Factors to numeric values
+immunisation$Y = as.numeric(levels(immunisation$Number.not.fully.immunised))[immunisation$Number.not.fully.immunised]
+immunisation$X = as.numeric(levels(immunisation$Number.of.registered.children))[immunisation$Number.of.registered.children]
+
+# Generating column with postcodes 1 to n corresponding to real postcodes in the neighborhood 
+# matrix This is nessesary for the Inla program to collect neighborhood information
+POAs_with_data$postcode_n = 1:nrow(POAs_with_data)
+immunisation = left_join(POAs_with_data, immunisation, by=c("POA_CODE"))
+
+#Generating extra column of postcodes for the BYM model
+immunisation = cbind(immunisation,postcode_struct=immunisation$postcode_n)
+
+# Model 1 ~ iid model specification
+formula2 = Y ~ f(postcode_n,model="iid")
+
+# Using Inla to compute the relative risk estimates
+result2  =  inla(formula2,family="binomial",data=immunisation,Ntrials=X, 
+                 control.compute = list(waic=TRUE, dic=TRUE, cpo=TRUE))
+
+# First look at relative risk ratio
+round(result2$summary.fitted.values[1:5,1],4)
+
+# Getting the IID relative risk estimates in dataframe for plotting
+iid_results = as.data.frame(t(ldply(result2$summary.fitted.values)[2:1080]))
+colnames(iid_results) = c("Mean", "sd", "0.25 quant", "0.5 quant","0.975 quant", "mode")
+
+# Adding column with postcode numbers
+iid_results$ID = seq.int(nrow(iid_results))
+
+
+# Collecting the Exeedence risk estimates from the iid model in a dataframe for plots
+a = 0 # Setting the threshold to 1 , log(1) = 0
+UHexceedence_iid <- lapply(result2$marginals.random$postcode_n, function(X){
+  1-inla.pmarginal(a, X) })
+UHexceedence_iid = ldply(UHexceedence_iid)
+
+# Adding column with postcodes numbers
+UHexceedence_iid$ID = seq.int(nrow(UHexceedence_iid))
+
+
+# Model 2 ~ BYM model specification, 2 components - the iid model and the besag model
+formula1 = Y ~ f(postcode_struct,model="besag",graph=g) +
+  f(postcode_n,model="iid")
+
+# Using Inla to find the relative risk estimates
+result1  =  inla(formula1,family="binomial",data=immunisation,Ntrials=X, 
+                 control.compute = list(waic=TRUE, dic=TRUE, cpo=TRUE))
+
+
+BYM_results = as.data.frame(t(ldply(result1$summary.fitted.values)[2:1080]))
+colnames(BYM_results) = c("Mean", "sd", "0.25 quant", "0.5 quant","0.975 quant", "mode")
+
+# Adding column with postcode numbers
+BYM_results$ID = seq.int(nrow(BYM_results))
+
+# Calculating the exeedence probability (the Posterior spatial risk is greater than 1)
+a = 0
+UHexceedence_BYM<-lapply(result1$marginals.random$postcode_struct, function(X){
+  1-inla.pmarginal(a, X) })
+UHexceedence_BYM = ldply(UHexceedence_BYM)
+
+# Adding column with postcode numbers
+UHexceedence_BYM$ID = seq.int(nrow(UHexceedence_BYM))
+
+# Comparring DIC and WAIC measures
+# Model evaluation parameters
+round(rbind(DIC=c(BYM=result1$dic$dic, IID=result2$dic$dic),
+            WAIC=c(result1$waic$waic, result2$waic$waic)),2)
+
 
 #################################### EXPORT MODEL DATA ############################################
 
